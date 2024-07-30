dbaaccb79edc1088aab889c350b8909045adc310
Sushant Kumar <sushantkumarab@gmail.com>
2024-07-30 21:28:56 +0530

    removed unreliable https api calls

--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -7,12 +7,12 @@ public class ApiService
 {
     public static async Task<Root> getWeather(double latitude, double longitude){
         var httpClient = new HttpClient();
-        var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid=f68cd597dccdc52bef388b4a9428087c",latitude,longitude));
+        var response = await httpClient.GetStringAsync(string.Format("http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid=f68cd597dccdc52bef388b4a9428087c",latitude,longitude));
         return JsonConvert.DeserializeObject<Root>(response);
     }
     public static async Task<Root> getWeatherCity(string cityName){
         var httpClient = new HttpClient();
-        var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/weather?q={0},in&APPID=f68cd597dccdc52bef388b4a9428087c",cityName));
+        var response = await httpClient.GetStringAsync(string.Format("http://api.openweathermap.org/data/2.5/weather?q={0},in&APPID=f68cd597dccdc52bef388b4a9428087c",cityName));
         return JsonConvert.DeserializeObject<Root>(response);
     }
 }
--- WeatherApp/obj/Debug/net8.0-android/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net8.0-android/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+327f151e5c7918ddd47e2a6ef5343ae401c2ef21")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
